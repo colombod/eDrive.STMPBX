2a246e177d550ee55131b10a5a3048b9220b6206
Diego Colombo <diego@mubreeze.com>
2013-07-07 16:21:24 +0100

    starting point for execution engine

--- eDrive/src/eDrive.cpp
+++ eDrive/src/eDrive.cpp
@@ -1,12 +1,44 @@
 #include "eDrive.hpp"
-using namespace mUBreeze;
-using namespace mUBreeze::eDrive;
 
-// default constructor
-Engine::Engine(){
+namespace mUBreeze{
+	namespace eDrive{
+
+		// default constructor
+		Engine::Engine(){
+		}
+
+		// the destructor is resposible to lcean up
+		Engine::~Engine(){
+			m_channelCount = 0;
+			ResetBuffers();
+		}
+
+		void Engine::Initialise(int samplingRate, int channelCount){
+			m_samplingRate = samplingRate;
+			m_channelCount = channelCount;
+			OnInitialising();
+		}
+
+		void Engine::OnInitialising(){
+	
+		}
+
+		void Engine::DoStep(){
+			if(m_channelCount > 0)
+			{
+			}
+	
+		}
+		
+		void Engine::ResetBuffers(){
+			m_incomingSamples.clear();
+			m_oututgoingSamples.clear();
+			
+			if(m_channelCount > 0)
+			{
+				m_incomingSamples.resize(m_channelCount, 0.0f);
+				m_oututgoingSamples.resize(m_channelCount, 0.0f);
+			}
+		}
+	}
 }
-
-// the destructor is resposible to lcean up
-Engine::~Engine(){
-}
-
