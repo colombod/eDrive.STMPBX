926c0c2411bbe36c2816a5d95c33796c85f5615f
Diego Colombo <diego@mubreeze.com>
2013-07-07 13:58:46 +0100

    Starting library structure

---
+++ eDrive/src/eDrive.cpp
@@ -0,0 +1,12 @@
+#include "eDrive.hpp"
+using namespace mUBreeze;
+using namespace mUBreeze::eDrive;
+
+// default constructor
+Engine::Engine(){
+}
+
+// the destructor is resposible to lcean up
+Engine::~Engine(){
+}
+
---
+++ eDrive/src/edrive.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace mUBreeze{
+	namespace eDrive{
+		class Engine{
+		public:
+			Engine();
+			virtual ~Engine();
+		};
+	}
+}
