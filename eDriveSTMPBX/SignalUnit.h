a6ba40b977649b64ed9a9432fc234996b5137246
Diego Colombo <diego@mubreeze.com>
2013-08-17 13:11:23 +0100

    Building structure

---
+++ eDriveSTMPBX/SignalUnit.h
@@ -0,0 +1,40 @@
+//
+//  SignalUnit.h
+//  eDrive
+//
+//  Created by Diego Colombo on 07/07/2013.
+//
+//
+
+#ifndef eDrive_SignalUnit_h
+#define eDrive_SignalUnit_h
+
+namespace mUBreeze{
+    namespace eDrive{
+        class Engine;
+        
+        /*
+         Base class to implement
+         */
+        class SignalUnit{
+        public:
+            SignalUnit();
+            virtual ~SignalUnit();
+            
+            void Initialise(Engine* engine);
+            
+        protected:
+            /*
+             Override for custom initialisation
+             */
+            void OnInitialising();
+        private:
+            Engine* m_engine;
+            
+            
+        };
+    }
+}
+
+
+#endif
